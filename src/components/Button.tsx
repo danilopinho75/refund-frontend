52e89c95bd20c4a47c0db04450bca161836eb57c
danilopinho75 <danilopinho11@gmail.com>
2026-02-28 12:00:36 -0300

    Organizando estrutura do componente Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,5 +3,14 @@ type Props = React.ComponentProps<"button"> & {
 }
 
 export function Button({ isLoading, children, type="button", ...rest }: Props) {
-  return <button type={type} {...rest} disabled={isLoading} className="flex items-center justify-center bg-green-100 rounded-lg text-white cursor-pointer hover:bg-green-200 transition ease-linear h-12 text-sm font-medium disabled:opacity-50 disabled:cursor-progress">{children}</button>
+  return (
+    <button 
+      className="flex items-center justify-center bg-green-100 rounded-lg text-white cursor-pointer hover:bg-green-200 transition ease-linear h-12 text-sm font-medium disabled:opacity-50 disabled:cursor-progress"
+      type={type} 
+      disabled={isLoading} 
+      {...rest} 
+    >
+      {children}
+    </button>
+    )
 }
