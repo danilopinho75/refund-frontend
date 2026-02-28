d4279953ec83a96e42da180cbca2fea663603cb1
danilopinho75 <danilopinho11@gmail.com>
2026-02-28 08:57:50 -0300

    Rota de autenticação

--- src/routes/auth-routes.tsx
+++ src/routes/auth-routes.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router';
 
 import { Signin } from '../pages/Signin';
+import { AuthLayout } from '../components/AuthLayout';
 
 export function AuthRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<Signin />} />
+      <Route path='/' element={<AuthLayout />}>
+        <Route path='/' element={<Signin />} />
+      </Route>
     </Routes>
   );
 }
